047ff67edfd245deaad056ca6d519445cd37b88a
Badjradaka <dakaherdinansyah@gmail.com>
2024-10-05 11:57:47 +0800

    Add files via upload

---
+++ C/PRAK002-2410817210029-BADJRADAKAHERDINANSYAHRAHARDJO.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (){
+    printf("Selamat Pagi, Badjradaka Herdinansyah Rahardjo \n");
+    printf("Selamat Siang, Badjradaka Herdinansyah Rahardjo \n");
+    printf("Selamat Malam, Badjradaka Herdinansyah Rahardjo \n");
+    return 0;
+}
---
+++ C/PRAK004-2410817210029-BADJRADAKAHERDINANSYAHRAHARDJO.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    printf("############################################ \n");
+    printf("#                                          # \n");
+    printf("#     Badjradaka Herdinansyah Rahardjo     # \n");
+    printf("#               2410817210029              # \n");
+    printf("#                                          # \n");
+    printf("############################################ \n");
+    return 0;
+}
